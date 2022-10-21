13cd6769c374c1ceff95798b287120245fcebf5e
Claudio Marques <claudiomarquesprogramador@gmail.com>
2022-10-21 18:04:02 -0300

    testar novo metodo de design dos modelos

---
+++ portfolio/script.js
@@ -0,0 +1,5 @@
+function iframeModelo1(){
+    if (document.getElementById('select-modelo-1').value == '1') {
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/1"
+    }
+}
