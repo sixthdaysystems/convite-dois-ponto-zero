8e33f3df6551cffe8a4be0b2e6ae4b9b4a4df214
Claudio Marques <claudiomarquesprogramador@gmail.com>
2022-10-21 18:55:31 -0300

    script tirei o vazio e mudei ordem dos modelos

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,15 +1,11 @@
 function iframeModelo1(){
     if (document.getElementById('select-modelo-1').value == '1') {
-        document.getElementById('modelo-site1').src="";
         document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/1";
     }else if (document.getElementById('select-modelo-1').value == '2') {
-        document.getElementById('modelo-site1').src="";
         document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/2";
     }else if (document.getElementById('select-modelo-1').value == '3') {
-        document.getElementById('modelo-site1').src="";
         document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/3";
     }else if (document.getElementById('select-modelo-1').value == '4') {
-        document.getElementById('modelo-site1').src="";
         document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/4";
     }
 };
