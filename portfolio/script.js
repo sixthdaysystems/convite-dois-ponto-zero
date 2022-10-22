0256cb90d24d4efc54188a2faf8dcb07ef959b9a
Claudio Marques <claudiomarquesprogramador@gmail.com>
2022-10-21 21:01:21 -0300

    ajuste nome tirando o numero

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,11 +1,13 @@
 function iframeModelo1(){
     if (document.getElementById('select-modelo-1').value == '1') {
-        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/1";
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-esquerda-grande";
     }else if (document.getElementById('select-modelo-1').value == '2') {
-        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/2";
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-direita-grande";
     }else if (document.getElementById('select-modelo-1').value == '3') {
-        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/3";
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-alto-grande";
     }else if (document.getElementById('select-modelo-1').value == '4') {
-        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/4";
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-embaixo-grande";
+    }else if (document.getElementById('select-modelo-1').value == '5') {
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-esquerda-medio";
     }
 };
