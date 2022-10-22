48ba9f45d1b0ddf9d9444e5ec1165421e4d1b200
Claudio Marques <claudiomarquesprogramador@gmail.com>
2022-10-21 23:21:36 -0300

    foto esquerda pequena adc

--- portfolio/script.js
+++ portfolio/script.js
@@ -8,12 +8,14 @@ function iframeModelo1(){
     }else if (document.getElementById('select-modelo-1').value == '4') {
         document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-embaixo-grande";
     }else if (document.getElementById('select-modelo-1').value == '5') {
-        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-esquerda-medio";
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-esquerda-media";
     }else if (document.getElementById('select-modelo-1').value == '6') {
-        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-direita-medio";
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-direita-media";
     }else if (document.getElementById('select-modelo-1').value == '7') {
-        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-alto-medio";
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-alto-media";
     }else if (document.getElementById('select-modelo-1').value == '8') {
-        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-embaixo-medio";
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-embaixo-media";
+    }else if (document.getElementById('select-modelo-1').value == '9') {
+        document.getElementById('modelo-site1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/modelos-sites/design-convite/modelo1/foto-esquerda-pequena";
     }
 };
