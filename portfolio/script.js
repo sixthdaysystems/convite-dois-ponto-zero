02a2c7805b3c6bf450c803b3874dc3b5149b6177
Claudio Marques <claudiomarquesprogramador@gmail.com>
2022-11-01 15:08:34 -0300

    update

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,8 +1,10 @@
 function iframe1(){
     if (document.getElementById('select-modelo-1').value == '1') {
-        document.getElementsByName('iframe1').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/flork/modelo1";
+        document.getElementById('iframe1-vertical').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/flork/modelo1";
+        document.getElementById('iframe1-horizontal').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/flork/modelo1";
     }else{
-        document.getElementsByClassName('iframe1').src="";
+        document.getElementById('iframe1-vertical').src="";
+        document.getElementById('iframe1-horizontal').src="";
     }
 }
 
