f1847a9cc954c9519d07f596ef8698d4ab93ac0c
Claudio Marques <claudiomarquesprogramador@gmail.com>
2022-11-21 23:13:40 -0300

    update

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,8 +3,10 @@ function iframe1(){
         document.getElementById('iframe1-vertical').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/tematico/instagram/index";
         document.getElementById('iframe1-horizontal').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/tematico/instagram/index";
     }else if(document.getElementById('select-modelo-1').value == '2'){
-        document.getElementById('iframe1-vertical').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/flork/modelo1";
-        document.getElementById('iframe1-horizontal').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/flork/modelo1";
+        //document.getElementById('iframe1-vertical').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/flork/modelo1";
+        //document.getElementById('iframe1-horizontal').src="https://sixthdaysystems.github.io/convite-dois-ponto-zero/flork/modelo1";
+        document.getElementById('iframe1-vertical').src="https://sixthdaysystems.github.io/aniversario/ravy/index";
+        document.getElementById('iframe1-horizontal').src="https://sixthdaysystems.github.io/aniversario/ravy/index";
     }else {
         document.getElementById('iframe1-vertical').src="";
         document.getElementById('iframe1-horizontal').src="";
